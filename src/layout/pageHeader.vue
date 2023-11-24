8bf49ceb5ea1edeae19b091bce1443b663799979
DsPan98 <60277682+DsPan98@users.noreply.github.com>
2023-11-24 21:57:59 +0800

    timezone updated

--- src/layout/pageHeader.vue
+++ src/layout/pageHeader.vue
@@ -15,7 +15,7 @@
             <div id="map" style="height:300px"></div>
         </div>
         <!-- section4 table here, with all the searched places -->
-        <div id="section4">Table here
+        <div id="section4" style="width:90%; align-self:center; margin:auto; border:2px solid red;">Table here
             <button @click="deleteSelected">button for deletion</button>
             <v-data-table :headers="headers" :items="places" :items-per-page="10" item-key="id" show-select
                 v-model="selected"></v-data-table>
@@ -24,7 +24,11 @@
 
         </div>
         <!-- section5 time zone here -->
-        <div id="section5">Timezone and local time here</div>
+        <div id="section5">
+            Timezone and local time here:
+            <div v-if="latestTimezone">Time Zone: {{ latestTimezone }}</div>
+            <div v-if="localTime">Local Time: {{ localTime }}</div>
+        </div>
     </div>
 </template>
 <script>
@@ -33,6 +37,7 @@ export default {
     mounted() {
         this.loadMapScript();
         this.loadTestItems();
+        setInterval(this.updateLocalTime, 1000);
     },
     methods: {
         getCurrentLocation() {
@@ -74,12 +79,14 @@ export default {
                             this.addMarker(location.lat, location.lng);
                             this.addPlace(this.markers[this.markers.length - 1], name, address);
                             this.map.setCenter(location);
+                            this.getTimeZone(location.lat, location.lng);
                             resolve();
                             //console.log('the markers array size after push is within fetch:', this.markers.length)
 
                         } else {
                             console.error('No results found');
                             reject(new Error('No results found'));
+                            alert('We could not find the location you typed in.');
                         }
                     })
                     .catch(error => {
@@ -175,12 +182,35 @@ export default {
                     address: '845 Rue Sherbrooke O, Montréal, QC H3A 0G4, Canada',
                     coords: '45.50478469999999, -73.5771511',
                 },
+                { id: -5, name: 'Place 5', address: 'Address 5', coords: '20.5, -20.5' },
+                { id: -6, name: 'Place 6', address: 'Address 6', coords: '21.5, -21.5' },
+                { id: -7, name: 'Place 7', address: 'Address 7', coords: '22.5, -22.5' },
+                { id: -8, name: 'Place 8', address: 'Address 8', coords: '23.5, -23.5' },
+                { id: -9, name: 'Place 9', address: 'Address 9', coords: '24.5, -24.5' },
+                { id: -10, name: 'Place 10', address: 'Address 10', coords: '25.5, -25.5' },
+                { id: -11, name: 'Place 11', address: 'Address 11', coords: '26.5, -26.5' },
+                { id: -12, name: 'Place 12', address: 'Address 12', coords: '27.5, -27.5' },
+                { id: -13, name: 'Place 13', address: 'Address 13', coords: '28.5, -28.5' },
+                { id: -14, name: 'Place 14', address: 'Address 14', coords: '29.5, -29.5' },
+                { id: -15, name: 'Place 15', address: 'Address 15', coords: '30.5, -30.5' },
+                { id: -16, name: 'Place 16', address: 'Address 16', coords: '31.5, -31.5' },
+                { id: -17, name: 'Place 17', address: 'Address 17', coords: '32.5, -32.5' },
+                { id: -18, name: 'Place 18', address: 'Address 18', coords: '33.5, -33.5' },
+                { id: -19, name: 'Place 19', address: 'Address 19', coords: '34.5, -34.5' },
+                { id: -20, name: 'Place 20', address: 'Address 20', coords: '35.5, -35.5' },
+                { id: -21, name: 'Place 21', address: 'Address 21', coords: '36.5, -36.5' },
+                { id: -22, name: 'Place 22', address: 'Address 22', coords: '37.5, -37.5' },
+                { id: -23, name: 'Place 23', address: 'Address 23', coords: '38.5, -38.5' },
+                { id: -24, name: 'Place 24', address: 'Address 24', coords: '39.5, -39.5' },
+                { id: -25, name: 'Place 25', address: 'Address 25', coords: '40.5, -40.5' },
             ];
         },
 
+        //this is used to add the current location into the table, update timezone, push items into places and markers array
         showAddress(lat, lng) {
             const geocodeUrl = `https://maps.googleapis.com/maps/api/geocode/json?latlng=${lat},${lng}&key=${this.getApiKey()}`;
             this.addMarker(lat, lng);
+            this.getTimeZone(lat, lng);
 
             fetch(geocodeUrl)
                 .then(response => response.json())
@@ -202,7 +232,32 @@ export default {
                 return null; // Or handle the error as needed
             }
             return process.env.VUE_APP_API_KEY;
-        }
+        },
+        getTimeZone(lat, lng) {
+            const timestamp = Math.round((new Date()).getTime() / 1000);
+            const timezoneUrl = `https://maps.googleapis.com/maps/api/timezone/json?location=${lat},${lng}&timestamp=${timestamp}&key=${this.getApiKey()}`;
+            fetch(timezoneUrl)
+                .then(response => response.json())
+                .then(timezoneData => {
+                    if (timezoneData.status === 'OK') {
+                        this.latestTimezone = timezoneData.timeZoneId;
+                        this.timezoneOffset = timezoneData.rawOffset;
+                        //console.log(this.latestTimezone, this.timezoneOffset);
+                        //console.log(new Date());
+                        //console.log(new Date((new Date()).getTime()));
+                        this.updateLocalTime();
+                    } else {
+                        console.error('Timezone data not found');
+                    }
+                })
+                .catch(error => console.error('Error:', error));
+        },
+        updateLocalTime() {
+            const currentTime = new Date(new Date().getTime() + new Date().getTimezoneOffset() * 60000);
+            const localTime = new Date(currentTime.getTime() + this.timezoneOffset * 1000);
+            this.localTime = localTime.toLocaleString();
+            //console.log(this.localTime);
+        },
     },
     computed: {
         tableMarkers() {
@@ -213,6 +268,9 @@ export default {
                 };
             });
         }
+    },
+    beforeDestroy() {
+        clearInterval(this.timeInterval);
     },
 
 
@@ -236,6 +294,9 @@ export default {
             selected: [],
             places: [],
 
+            latestTimezone: '',
+            timezoneOffset: 0,
+            localTime: '',
         };
     }
 }
